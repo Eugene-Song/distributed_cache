48db3f8200d2a379415d67b36e2a2b3e2145fb25
Yuxin Song <ys385@duke.edu>
2024-09-10 14:14:56 -0700

    implemnt consistent hash for the input key to nodes

---
+++ consistenthash.go
@@ -0,0 +1,69 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map contains all hashed keys
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int
+	hashMap  map[int]string
+}
+
+// New creates a Map instance
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// Add adds some keys to the hash, add nodes to the ring
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			// name virtual node as index + key, and do the hash
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+
+			// add virtual node hash to the ring
+			m.keys = append(m.keys, hash)
+
+			// add virtual node read node mapping to the map
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+// Get the node by a key
+func (m *Map) Get(key string) string {
+	// no nodes
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	// hash the key
+	hash := int(m.hash([]byte(key)))
+
+	// find the closest virtual node in the ring
+	// binary search to get the closest virtual node
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+
+	// get the real node name
+	// for handling edge case that the idx == len(m.keys), use mod to get the first virtual node
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
---
+++ consistenthash_test.go
@@ -0,0 +1,50 @@
+package consistenthash
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestHashing(t *testing.T) {
+	hash := New(3, func(key []byte) uint32 {
+		i, _ := strconv.Atoi(string(key))
+		return uint32(i)
+	})
+
+	// Given the above hash function, this will give replicas with "hashes":
+	// when add 6 with three replicas, the virtual nodes will be 6, 16, 26
+	// when add 4 with three replicas, the virtual nodes will be 4, 14, 24
+	// when add 2 with three replicas, the virtual nodes will be 2, 12, 22
+	hash.Add("6", "4", "2")
+
+	// for _, key := range hash.keys {
+	// 	t.Logf("%v", key)
+	// }
+	// print(hash.hashMap)
+
+	testCases := map[string]string{
+		"2":  "2",
+		"11": "2",
+		"23": "4",
+		"27": "2",
+	}
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s. But get %s", k, v, hash.Get(k))
+		}
+	}
+
+	// Adds 8, 18, 28
+	hash.Add("8")
+
+	// 27 should now map to 8.
+	testCases["27"] = "8"
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s. But get %s", k, v, hash.Get(k))
+		}
+	}
+
+}
